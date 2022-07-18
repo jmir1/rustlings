c09fcaf2f1a01d0175a0857194b68dfd25efa1f5
jmir1 <jhmiramon@gmail.com>
2022-07-18 22:54:16 +0200

    solve exercises

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,8 +18,6 @@
 // - The output element is going to be a Vector of strings.
 // Execute `rustlings hint quiz2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -27,14 +25,30 @@ pub enum Command {
 }
 
 mod my_module {
-    use super::Command;
+    use super::{ Command, Command::{ Uppercase, Trim, Append } };
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Uppercase => {
+                    output.push(string.to_uppercase());
+                },
+                Trim => {
+                    output.push(string.trim().to_string());
+                },
+                Append(size) => {
+                    let mut string = string.to_string();
+                    let size = *size;
+                    for i in 0..size {
+                        string += "bar";
+                    }
+                    output.push(string.to_string());
+                },
+            };
         }
         output
     }
@@ -43,7 +57,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What to we have to import to have `transformer` in scope?
-    use ???;
+    use my_module::transformer;
     use super::Command;
 
     #[test]
